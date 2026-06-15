06945ba68b687c4e5fdae7c3c587fe102f74593d
hector <hectorcaballero1205@gmail.com>
2026-06-15 05:41:58 -0500

    add dynamo region

--- common/dynamo.py
+++ common/dynamo.py
@@ -2,7 +2,7 @@ import os
 import boto3
 from boto3.dynamodb.conditions import Key
 
-dynamodb = boto3.resource("dynamodb")
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
 
 
 def get_table(name):
